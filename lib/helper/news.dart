8692c1fd0cf95efa816c9e638a04e7cfb34f478e
Enes Polat <37534266+enespolat25@users.noreply.github.com>
2020-05-18 00:48:00 +0300

    Update news.dart

--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -10,7 +10,7 @@ class News {
 
   Future<void> getNews() async{
 
-    String url = "http://newsapi.org/v2/top-headlines?country=in&excludeDomains=stackoverflow.com&sortBy=publishedAt&language=en&apiKey=${apiKey}";
+    String url = "http://newsapi.org/v2/top-headlines?country=tr&excludeDomains=stackoverflow.com&sortBy=publishedAt&language=en&apiKey=${apiKey}";
 
     var response = await http.get(url);
 
@@ -49,7 +49,7 @@ class NewsForCategorie {
   Future<void> getNewsForCategory(String category) async{
 
     /*String url = "http://newsapi.org/v2/everything?q=$category&apiKey=${apiKey}";*/
-    String url = "http://newsapi.org/v2/top-headlines?country=in&category=$category&apiKey=${apiKey}";
+    String url = "http://newsapi.org/v2/top-headlines?country=tr&category=$category&apiKey=${apiKey}";
 
     var response = await http.get(url);
 
